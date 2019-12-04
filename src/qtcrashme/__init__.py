252806ab8d9dc59ba7d36b49ed1ac93ff5140596
Tiago Montes <tiago.montes@gmail.com>
2019-12-04 12:36:15 +0000

    Less code, still crashing.

--- src/qtcrashme/__init__.py
+++ src/qtcrashme/__init__.py
@@ -1,10 +1,10 @@
-__version__ = '1.0.0a0'
+__version__ = "1.0.0a0"
 
-__title__ = 'qtcrashme'
-__description__ = 'Qt Crash Friendly Test'
+__title__ = "qtcrashme"
+__description__ = "Qt Crash Friendly Test"
 
-__license__ = 'MIT'
-__uri__ = 'https://github.com/tmontes/qtcrashme/'
+__license__ = "MIT"
+__uri__ = "https://github.com/tmontes/qtcrashme/"
 
-__author__ = 'Tiago Montes'
-__email__ = 'tiago.montes@gmail.com'
+__author__ = "Tiago Montes"
+__email__ = "tiago.montes@gmail.com"
--- src/qtcrashme/__main__.py
+++ src/qtcrashme/__main__.py
@@ -1,73 +1,51 @@
 import sys
 
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (
-    QApplication, QLabel, QLineEdit, QMainWindow, QPushButton, QVBoxLayout
-)
-
+from PyQt5 import QtWidgets
 import qcrash.api as qcrash
 
 
-class MainWindow(QMainWindow):
-
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
 
         super().__init__()
 
-        self.setWindowTitle('Qt Crash Friendly Test')
-        self.resize(320, 240)
+        self.setWindowTitle("Qt Crash Friendly Test")
+        self.resize(320, 70)
 
-        self.python = QLineEdit(self)
-        self.python.move(20, 20)
-        self.python.resize(280, 20)
+        self.button = QtWidgets.QPushButton("Click to crash", self)
+        self.button.move(20, 20)
+        self.button.resize(280, 30)
 
-        self.label = QLabel('(submit a Python expression)', self)
-        self.label.setAlignment(Qt.AlignTop | Qt.AlignLeft)
-        self.label.setWordWrap(True)
-        self.label.move(20, 50)
-        self.label.resize(280, 170)
+        self.button.clicked.connect(self.raise_exception)
 
-        self.python.editingFinished.connect(self.evaluate_python)
+    def raise_exception(self):
 
-
-    def evaluate_python(self):
-
-        globals = {}
-        locals = {}
-        python = self.python.text().strip()
-        if python:
-            result = eval(python, globals, locals)
-            output = str(result)
-        else:
-            output = 'empty Python expression'
-        self.update_label(output)
-
-    def update_label(self, text):
-        self.label.setText(text)
-        self.label.repaint()
+        raise BaseException("root of the exception hierarchy")
 
 
 def setup_qcrash():
 
-    qcrash.get_application_log = lambda: 'application log\nline1\nline2'
-    qcrash.get_system_information = lambda: 'system info here'
+    github = qcrash.backends.GithubBackend("tmontes", "qtcrashme")
+    qcrash.install_backend(github)
 
-    github = qcrash.backends.GithubBackend('tmontes', 'qtcrashme')
-    email = qcrash.backends.EmailBackend('tiago.montes@gmail.com', 'qtcrashme')
-    qcrash.install_backend([github, email])
+    email = qcrash.backends.EmailBackend("user@example.com", "qtcrashme")
+    qcrash.install_backend(email)
+
+    qcrash.get_application_log = lambda: "line1\nline2\n"
+    qcrash.get_system_information = lambda: "system information here"
 
     qcrash.install_except_hook()
 
 
 def main():
 
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     setup_qcrash()
     win = MainWindow()
     win.show()
     sys.exit(app.exec_())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     main()
